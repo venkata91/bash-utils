5e1607759f9c4f181fb0e41ca9f10f210debccd4
venkata91 <vsowrira@asu.edu>
2015-06-22 19:43:37 -0700

    Sleepsort works faster than before by just sleeping for log to the base 10 of input

--- sleepsort.sh
+++ sleepsort.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 function f() {
-    x='l("$1")/l(10)' | bc -l
-    echo "$x"
-    echo "$1"
+   #http://askubuntu.com/questions/229446/how-to-pass-results-of-bc-to-a-variable
+   s=$(bc -l <<< "l($1)/l(10)")
+   #echo "Sleeping for $s secs"
+   sleep "$s" 
+   echo "$1"
 }
 while [ -n "$1" ]
 do
